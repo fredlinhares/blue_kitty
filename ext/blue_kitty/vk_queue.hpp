5b57e49831de0b9c6a9222bdd21885794bf6e5bc
Frederico Linhares <fred@linhares.blue>
2019-09-23 20:15:22 -0300

    fixt Correct parameter for queue submitted one time
    * ext/blue_kitty/vk_queue.hpp (Queue::submit_one_time_command):
    VkSubmitInfo.pCommandBuffers expects a pointer, not a variable.

--- ext/blue_kitty/vk_queue.hpp
+++ ext/blue_kitty/vk_queue.hpp
@@ -55,7 +55,7 @@ template<typename T>
 void Queue::submit_one_time_command(const VkCommandBuffer vk_command_buffer,
                                     T commands)
 {
-  VkCommandBufferBeginInfo buffer_begin_info = {};
+  VkCommandBufferBeginInfo buffer_begin_info{};
   buffer_begin_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
   buffer_begin_info.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
 
@@ -65,14 +65,14 @@ void Queue::submit_one_time_command(const VkCommandBuffer vk_command_buffer,
 
   vkEndCommandBuffer(vk_command_buffer);
 
-  VkSubmitInfo submit_info = {};
+  VkSubmitInfo submit_info{};
   submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
   submit_info.pNext = nullptr;
   submit_info.waitSemaphoreCount = 0;
   submit_info.pWaitSemaphores = nullptr;
   submit_info.pWaitDstStageMask = nullptr;
   submit_info.commandBufferCount = 1;
-  submit_info.pCommandBuffers = vk_command_buffer;
+  submit_info.pCommandBuffers = &vk_command_buffer;
   submit_info.signalSemaphoreCount = 0;
   submit_info.pSignalSemaphores = nullptr;
 
